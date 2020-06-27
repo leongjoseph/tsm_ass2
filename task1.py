933cd1cbd5e8300b3da01bcc1e1dfc568bc95c19
Joseph Leong <jleong2@student.unimelb.edu.au>
2020-06-27 15:55:24 +1000

    MSA started

--- task1.py
+++ task1.py
@@ -21,12 +21,14 @@ from pprint import pprint
 
 DATA_PATH = Path(__file__).parent / 'Data'
 
+
 class Data:
     def __init__(self):
         self.link_data = self._open_and_generate_link_data()
         self.od_matrix = np.genfromtxt(DATA_PATH / 'od_matrix_drive.csv', delimiter=',')
-        self.od_list = {}
         self.update_tt({link: 0 for link in self.link_data['link_id']})
+        self.adjacency_list = self._get_adjacency_list()
+        self.verticies = self._get_verticies()
 
     def _open_and_generate_link_data(self):
         raw_link_data = pd.read_csv(DATA_PATH / 'links.csv')
@@ -53,8 +55,39 @@ class Data:
 
         return link_actual_tt
 
+    def _get_adjacency_list(self):
+        # 'adjacency' refers to the ability to travel TO a vertex FROM another vertex
+        adjacency_list = {}
 
-class Dijkstra:
+        for link_data in self.link_data.itertuples():
+            starting_node = link_data[2]
+            ending_node = link_data[3]
+
+            current_list = adjacency_list.get(starting_node)
+
+            if current_list is None:
+                adjacency_list.update({
+                    starting_node: [ending_node]
+                })
+            else:
+                current_list.append(ending_node)
+
+        return adjacency_list
+
+    def _get_verticies(self):
+        verticies = set()
+
+        for link_data in self.link_data.itertuples():
+            starting_node = link_data[2]
+            ending_node = link_data[3]
+
+            verticies.add(starting_node)
+            verticies.add(ending_node)
+
+        return list(verticies)
+
+
+class Dijkstra(Data):
     """
         1. Let distance of start vertex from start vertex = 0
         2. Let the distance of all other vertices from start = infinity
@@ -69,61 +102,25 @@ class Dijkstra:
             6. Add the current vertex to the list of visited vertices
     """
 
-    def __init__(self, data):
-        self.data = data
-        self.verticies = self._get_verticies()
-        self.adjacency_list = self._get_adjacency_list()
+    def __init__(self):
+        Data.__init__(self)
 
-    def _initialise_travel_times(self, data):
-        pass
-        
-    def _get_adjacency_list(self):
-        # 'adjacency' refers to the ability to travel TO a vertex FROM another vertex
-        adjacency_list = {}
-        
-        for link_data in self.data.itertuples():
-            starting_node = link_data[2]
-            ending_node = link_data[3]
-            
-            current_list = adjacency_list.get(starting_node)
-            
-            if current_list is None:
-                adjacency_list.update({
-                    starting_node: [ending_node]
-                    })
-            else:
-                current_list.append(ending_node)
-        
-        return adjacency_list
- 
-    def _get_verticies(self):
-        verticies = set()
-        
-        for link_data in self.data.itertuples():
-            starting_node = link_data[2]
-            ending_node = link_data[3]
-            
-            verticies.add(starting_node)
-            verticies.add(ending_node)
-        
-        return list(verticies)
-
-    def calculate(self, origin_vertex):
+    def calculate_tree(self, origin_vertex):
         unvisited_verticies = self.verticies.copy()
         visited_verticies = []
         tt_from_origin = 0
-        
-        # initialise dictionary for values = {vertex: [min_travel_time, prior_vertex]}
-        known_values = {vertex: [np.inf, None] for vertex in unvisited_verticies if vertex is not origin_vertex}
-        known_values.update({origin_vertex: [0, None]})
-        
-        def _calculate_recursion(starting_vertex, tt_from_origin):
-            # Recursion end condition
-            if len(unvisited_verticies) == 0:
-                return
+        current_path = []
 
+        # initialise dictionary for values = {vertex: [min_travel_time, prior_vertex]}
+        self.known_values = {vertex: {'tt': np.inf,
+                                 'path': None} for vertex in unvisited_verticies if vertex is not origin_vertex}
+
+        self._calculate_update_values(origin_vertex, tt=0, path=None)
+
+        def _calculate_recursion(starting_vertex, tt_from_origin, current_path):
             visited_verticies.append(starting_vertex)
             unvisited_verticies.remove(starting_vertex)
+            current_path.append(starting_vertex)
 
             # Get neighbours of the current vertex
             neighbours = self.adjacency_list.get(starting_vertex)
@@ -136,13 +133,13 @@ class Dijkstra:
                     neighbours_values.update({
                         neighbour: tt
                         })
-                    
-                    current_value = known_values.get(neighbour)
 
-                    if tt + tt_from_origin < current_value[0]:
-                        known_values.update({
-                            neighbour: [tt + tt_from_origin, starting_vertex]
-                            })
+                    current_value = self.known_values.get(neighbour)
+
+                    if tt + tt_from_origin < current_value['tt']:
+                        self._calculate_update_values(neighbour,
+                                                      tt + tt_from_origin,
+                                                      current_path.copy())
 
             # Condition for no unvisited neighbours
             if len(neighbours_values) != 0:
@@ -150,27 +147,58 @@ class Dijkstra:
                 closest_vertex_tt = min(neighbours_values.values())
                 tt_from_origin += closest_vertex_tt
                 
-                return _calculate_recursion(closest_vertex, tt_from_origin)
+                return _calculate_recursion(closest_vertex, tt_from_origin, current_path)
         
-        _calculate_recursion(origin_vertex, tt_from_origin)
-        
-        return known_values, tt_from_origin
-            
+        _calculate_recursion(origin_vertex, tt_from_origin, current_path)
+
+        return self.known_values
+
+    def _calculate_update_values(self, dest_vertex, tt, path):
+        self.known_values.update({dest_vertex: {
+            'tt': tt,
+            'path': path
+        }})
+
     def _calculate_get_tt(self, orig_vertex, dest_vertex):
-        travel_time = self.data['actual_tt'].loc[
-            (self.data['init_node'] == orig_vertex) & (self.data['term_node'] == dest_vertex)].item()
+        travel_time = self.link_data['actual_tt'].loc[
+            (self.link_data['init_node'] == orig_vertex) & (self.link_data['term_node'] == dest_vertex)].item()
         
         return travel_time
 
-    def calculate_shortest_route(self, origin, destination):
+
+class MSA(Dijkstra):
+    def __init__(self):
+        super().__init__()
+
+    def _get_route(self, orig, dest):
+        tree = self.calculate_tree(orig)
+        dest_values = tree.get(dest)
+        path_to_dest = dest_values['path']
+        path_links = self._get_route_convert_to_link(path_to_dest, dest)
+
+    def _get_route_convert_to_link(self, path, dest):
+        if len(path) == 1:
+            path_links = [tuple([path[0], dest])]
+        else:
+            path_links = []
+            for idx, vertex in enumerate(path):
+                if idx == 0:
+                    pass
+                elif idx == len(path) - 1:
+                    path_links.append(tuple([path[idx - 1], path[idx]]))
+                    path_links.append(tuple([path[idx], dest]))
+                else:
+                    path_links.append(tuple([path[idx - 1], vertex]))
+
+        return path_links
+
+    def _assign_demand(self):
         pass
-        
+
 
 if __name__ == "__main__":
-    data = Data()
-    dijkstra, tt = Dijkstra(data.link_data).calculate(1)
-    pprint(dijkstra)
-    dijkstra = Dijkstra(data.link_data).calculate(2)
-    pprint(dijkstra)
+    msa = MSA()
+    msa._get_route(1, 13)
+
 
 
